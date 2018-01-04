0739df838d11f9117ab547f3f8ef4c8fafb6c1e3
David <dmolina101@gmail.com>
2018-01-04 07:02:22 -0400

    Upgrade

--- modulos/dmolina101/js/index.js
+++ modulos/dmolina101/js/index.js
@@ -3,4 +3,22 @@ $(document).ready(function(){
   //Para los tooltips
   $('[data-toggle="tooltip"]').tooltip()
 
+  $('#html, #css3, #js, #less, #php, #restful, #codeigniter, #jquery').barrating({
+    initialRating : 4,
+    readonly      : true,
+    theme         : 'bars-movie'
+  });
+
+  $('#mysql, #node, #oracle, #bootstrap, #soap, #git').barrating({
+    initialRating : 3,
+    readonly      : true,
+    theme         : 'bars-movie'
+  });
+
+  $('#dynamodb, #laravel, #android, #composer').barrating({
+    initialRating : 2,
+    readonly      : true,
+    theme         : 'bars-movie'
+  });
+
 });//Fin del document ready
