5147e81a52e81fbf6fc1739751011aea09459a7d
David <dmolina101@gmail.com>
2018-01-29 08:21:40 -0430

    Update Ranking Bootstrap
    Actualice el ranking de la habilidad Boostrap

--- modulos/dmolina101/js/index.js
+++ modulos/dmolina101/js/index.js
@@ -5,13 +5,13 @@ $(document).ready(function(){
   //Para los tooltips
   $('[data-toggle="tooltip"]').tooltip()
 
-  $('#html, #css3, #js, #less, #php, #restful, #codeigniter, #jquery').barrating({
+  $('#html, #css3, #js, #less, #php, #restful, #codeigniter, #jquery, #bootstrap').barrating({
     initialRating : 4,
     readonly      : true,
     theme         : 'bars-movie'
   });
 
-  $('#mysql, #node, #oracle, #bootstrap, #soap, #git').barrating({
+  $('#mysql, #node, #oracle, #soap, #git').barrating({
     initialRating : 3,
     readonly      : true,
     theme         : 'bars-movie'
